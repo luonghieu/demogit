0db229ecc6dd2f009aa43b889adc12c7544e95ec
luonghieu <luonghieu.t3@gmail.com>
2017-10-07 17:52:32 +0700

    Update config.php

--- Our-Shop-master/config.php
+++ Our-Shop-master/config.php
@@ -2,8 +2,8 @@
 	return [
 		'database'=>[
 			'age' =>'mytodo',
-			'username'=>'root',
-			'password'=>'123456',
+			'abc'=>'root',
+			'bcd'=>'123456',
 			'connection'=>'mysql:host=127.0.0.1',
 			'options'=>[
 				PDO::ATTR_ERRMODE =>PDO::ERRMODE_EXCEPTION
